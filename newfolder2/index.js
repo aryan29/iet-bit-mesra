fd2159fa9d1421aaca9475991f13138be9d84d45
Aryan Khandelwal <nk28agra@gmail.com>
2019-02-21 19:41:31 +0530

    second commit

---
+++ newfolder2/index.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+$(document).ready(function(){
+console.log("I am ready");                                                   //get ready loads javascript after document is ready
+}); //document selects whole html document
