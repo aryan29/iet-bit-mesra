2ca90d0ad4010f4ba9ea6c14554d9b356829cc73
Aryan Khandelwal <nk28agra@gmail.com>
2019-02-22 19:39:20 +0530

    last commit

--- newfolder2/index.js
+++ newfolder2/index.js
@@ -1,4 +1,26 @@
-console.log("Hello World");
 $(document).ready(function(){
-console.log("I am ready");                                                   //get ready loads javascript after document is ready
-}); //document selects whole html document
+    $("#list").css({
+        "background-color" : "#510d51",
+        "font-size" : "20px",
+        "color" : "#fff"
+    });
+});
+setTimeout(function(){
+    document.getElementById("list").innerHTML=("<ul><li>hey buddy</li><li>hello</li></ul>");
+},15000);
+let fontItem="";
+for(var i=15;i<50;i++){
+    console.log("hello");
+    fontItem= '<li id="font'+i+'">I am font size:'+i+'</li>';
+    red=Math.floor(Math.random()*256);
+    blue=Math.floor(Math.random()*256);
+    green=Math.floor(Math.random()*256);
+    $("#list").append(fontItem);
+    $("#font"+i).css({
+        "background-color": "rgb("+red+","+blue+","+green+")",
+        "font-size": i+"px"
+    })
+}
+
+  
+
